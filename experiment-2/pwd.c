27b2b42c607849f992b6ec74e1e3a652001c7a42
GaoSong <shimaring@qq.com>
2022-12-16 15:17:38 +0800

    feat: implement pwd in primitive way

--- experiment-2/pwd.c
+++ experiment-2/pwd.c
@@ -4,15 +4,45 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
 
 int main() {
-    char abspath[1024];
-    const char *currentPath = ".";
-    char *point = realpath(currentPath, abspath);
-    if (point == NULL) {
-        perror("calculate absolute path fail");
-        exit(EXIT_FAILURE);
+    //get current dir .
+    //keep access
+    //record the final path
+    char *result[255];
+    int count = 0;
+    DIR *dir;
+    struct dirent *p;
+    struct stat statbuf;
+    while (1) {
+        stat(".", &statbuf);
+        u_int32_t curNodeNumber = statbuf.st_ino;
+        stat("..", &statbuf);
+        u_int32_t parentNodeNumber = statbuf.st_ino;
+        if (curNodeNumber == parentNodeNumber) {
+            break;
+        }
+        //else attempt to get the filename
+        chdir("..");
+        dir = opendir(".");
+        while (NULL != (p = readdir(dir))) {
+            if (p->d_ino == curNodeNumber) {
+                result[count] = malloc(strlen(p->d_name) + 1);
+                strcpy(result[count++], p->d_name);
+                break;
+            }
+        }
+        closedir(dir);
+        //chdir to parent
     }
-    printf("\n%s\n", abspath);
+    //reverse and print
+    printf("/");
+    for (count = count - 1; count >= 0; count--) {
+        printf("%s/", result[count]);
+    }
     return 0;
 }
